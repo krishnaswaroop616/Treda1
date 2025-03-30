d4e224abb3be449e02a456b35961e2893f0ee131
krishnaswaroop616 <kswaroop311@gmail.com>
2025-03-30 19:48:54 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,7 +20,7 @@ import Login from './landing_page/signup/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/">
 
     <Routes>
       <Route path='/' element={<HomePage />}></Route>
