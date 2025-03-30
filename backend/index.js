08fde4e314adaec3d6bd0a812c03fdf6f52fc391
krishnaswaroop616 <kswaroop311@gmail.com>
2025-03-30 19:21:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -68,7 +68,7 @@ app.post("/login", async (req, res) => {
         }
 
         const token = jwt.sign({ userId: User._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
-        res.cookie("token", token, { httpOnly: true, secure: true, sameSite: "None" });
+        res.cookie("token", token, { httpOnly: true, secure: true, sameSite: "None",maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days });
         res.status(200).json({ message: "Login successful", user: { username: User.username, email: User.email } });
     }
     catch (err) {
